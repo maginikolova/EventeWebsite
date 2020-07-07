bf9e9e05c611f61cac66b660b4b4ac7415e8ad9d
Teya Andreeva <teyaandreeva@gmail.com>
2020-07-07 22:25:56 +0300

    Removed unused files

--- Event Organizer PHP/admin/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-try
-{
-	$pdo = new PDO('mysql:host=localhost;dbname=autonexus;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
--- Event Organizer PHP/includes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try{
-	$pdo = new PDO('mysql:host=localhost;dbname=autonexus','root','');
-} catch (PDOException $e){
-	exit('DB error. Fix SQL');
-}
-
-?>
